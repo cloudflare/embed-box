3c9b8862f782a763a272627a4f0e667c93421fa4
Teffen Ellis (Eric) <teffen@nirri.us>
2016-06-22 04:58:42 -0700

    Add SVG icons.

---
+++ app/components/icons/index.js
@@ -0,0 +1,16 @@
+import "./icons.styl"
+
+import React from "react"
+import InlineSVG from "svg-inline-react"
+
+import drupalSVG from "./drupal.svg"
+export const drupal = <InlineSVG className="inline-svg" src={drupalSVG} />
+
+import embedSVG from "./embed.svg"
+export const embed = <InlineSVG className="inline-svg" src={embedSVG} />
+
+import joomlaSVG from "./joomla.svg"
+export const joomla = <InlineSVG className="inline-svg" src={joomlaSVG} />
+
+import wordpressSVG from "./wordpress.svg"
+export const wordpress = <InlineSVG className="inline-svg" src={wordpressSVG} />
