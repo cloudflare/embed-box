df2b217de0ef2bd057ff73623a613b4b44542d8f
Teffen Ellis (Eric) <teffen@nirri.us>
2016-06-22 04:58:58 -0700

    Flesh out app layout from mockup. WIP

--- app/containers/application/index.js
+++ app/containers/application/index.js
@@ -1,7 +1,35 @@
+import "./application.styl"
+
 import React from "react"
+import SiteTypeSearch from "containers/site-type-search"
 
 export default class Application extends React.Component {
+  state = {
+    appName: "Drift Chat",
+    types: [
+      "WordPress",
+      "Drupal",
+      "Joomla",
+      "Embed"
+    ]
+  };
+
   render() {
-    return <div>Hello world!</div>
+    const {appName, types} = this.state
+
+    return <section data-column data-component="containers/application">
+      <div className="modal" data-column>
+        <div className="header">
+          <button data-action="previous">PP</button>
+
+          <span>Add {appName} to your site</span>
+
+          <button data-action="close">CC</button>
+        </div>
+
+        <SiteTypeSearch types={types} />
+
+      </div>
+    </section>
   }
 }
