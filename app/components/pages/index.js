6bd0d837139ca8aee5a95a604259ac34e32fb02b
Teffen Ellis (Eric) <teffen@nirri.us>
2016-06-28 07:24:59 -0700

    Flesh out Joomla page.

--- app/components/pages/index.js
+++ app/components/pages/index.js
@@ -1,3 +1,4 @@
 import "./pages.styl"
 
+export joomla from "./joomla"
 export wordpress from "./wordpress"
---
+++ app/components/pages/joomla/index.js
@@ -0,0 +1,6 @@
+import BaseComponent from "components/base-component"
+import template from "./joomla.pug"
+
+export default class JoomlaPage extends BaseComponent {
+  template = template;
+}
