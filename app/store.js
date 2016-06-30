3244aac45efe2f8698a49e80ff787aa22c649323
Teffen Ellis (Eric) <teffen@nirri.us>
2016-06-30 09:26:44 -0700

    Move labels to configuration.

--- app/store.js
+++ app/store.js
@@ -1,15 +1,25 @@
 export default {
   appName: "Drift Chat",
   siteId: "Icc0-PIkXF",
+
+  selectedId: "",
   page: "home",
+
   types: [
     {label: "WordPress", id: "wordpress"},
     {label: "Drupal", id: "drupal"},
     {label: "Joomla", id: "joomla"},
     {label: "Embed", id: "embed"}
   ],
-  selectedId: "",
+
+
   accentColor: "#2d88f3",
   textColor: "#000000",
-  backgroundColor: "#ffffff"
+  backgroundColor: "#ffffff",
+
+  labels: {
+    done: "Done",
+    searchPlaceholder: "Select or search the type of website you have...",
+    next: "Next"
+  }
 }
