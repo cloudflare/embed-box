7c953174a8748e10db102757b84c63033a9d920b
Teffen Ellis (Eric) <teffen@nirri.us>
2016-08-08 07:20:58 -0700

    Pragmatically build target list.

--- modules/index.js
+++ modules/index.js
@@ -1,8 +1,14 @@
 /* eslint-env node */
 
 const base = "./modules/"
+const targetsPath = "./modules/targets/"
+const fs = require("fs")
 
-exports.entries = {
+const IDs = fs
+  .readdirSync(targetsPath)
+  .map(entry => entry.split(".js")[0])
+
+const entries = {
   "embed-box": {
     library: "EmbedBox",
     path: base + "embed-box.js"
@@ -14,23 +20,16 @@ exports.entries = {
   "embed-box-custom-target": {
     library: "EmbedBoxCustomTarget",
     path: base + "custom-target.js"
-  },
-  "embed-box-target-drupal": {
-    library: "EmbedBoxDrupalTarget",
-    path: base + "targets/drupal.js"
-  },
-  "embed-box-target-generic": {
-    library: "EmbedBoxGenericTarget",
-    path: base + "targets/generic.js"
-  },
-  "embed-box-target-joomla": {
-    library: "EmbedBoxWordpressTarget",
-    path: base + "targets/joomla.js"
-  },
-  "embed-box-target-wordpress": {
-    library: "EmbedBoxWordpressTarget",
-    path: base + "targets/wordpress.js"
   }
 }
+
+IDs.forEach(id => {
+  entries[`embed-box-target-${id}`] = {
+    library: `EmbedBox${id}Target`,
+    path: `${targetsPath}${id}.js`
+  }
+})
+
+exports.entries = entries
 
 exports.IDs = Object.keys(exports.entries)
