ac89179432389849f4b0a657f26d32663448bf41
Teffen Ellis (Eric) <teffen@nirri.us>
2016-08-11 06:59:44 -0700

    Run first demo on load.
    Closes #38

--- app/site/index.js
+++ app/site/index.js
@@ -31,7 +31,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const toc = document.querySelector(".table-of-contents")
   const docsNav = document.querySelector(".docs-nav > .sticky")
   const demoWrapper = document.querySelector(".demo-wrapper > .sticky")
-  const runInlineContainer = document.getElementById("run-inline-container")
 
   Array
     .from(document.querySelectorAll("h2.headline-with-anchor [name], h3.headline-with-anchor [name]"))
@@ -60,14 +59,15 @@ document.addEventListener("DOMContentLoaded", () => {
     EmbedBoxCustom: window.EmbedBoxCustom
   }
   const automatedFrame = document.getElementById("automated-frame")
-  let previousInstance = null
+  const runInlineContainer = document.getElementById("run-inline-container")
+  let demoInstance = null
   let createInteractiveDemo
 
   function bindObjectArguments(Constructor, boundSpec = {}) {
     return spec => {
-      previousInstance = new Constructor({...boundSpec, ...spec})
+      demoInstance = new Constructor({...boundSpec, ...spec})
 
-      return previousInstance
+      return demoInstance
     }
   }
 
@@ -77,21 +77,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
   loadDemoScripts(automatedFrame.contentDocument, loopRunDemo)
 
-  function evalRunButton(button) {
-    const {parentElement} = button
-    const useModal = button.getAttribute("data-run") === "modal"
-    const container = useModal ? document.body : runInlineContainer
-    const example = parentElement.querySelector("code").innerText
-
-
-    if (previousInstance) previousInstance.destroy()
-
+  function stopDemoLoop() {
     if (createInteractiveDemo) {
       // The hero automated demo takes focus from other inputs.
       // Switching to the interactive demo prevents this.
       createInteractiveDemo()
       createInteractiveDemo = null
     }
+  }
+
+  function evalRunButton(button) {
+    const {parentElement} = button
+    const useModal = button.getAttribute("data-run") === "modal"
+    const container = useModal ? document.body : runInlineContainer
+    const example = parentElement.querySelector("code").innerText
+
+    if (demoInstance) demoInstance.destroy()
+
+    stopDemoLoop()
 
     // Clear previous demo routing.
     window.history.pushState("", "", window.location.pathname)
@@ -102,7 +105,13 @@ document.addEventListener("DOMContentLoaded", () => {
     window.eval(example) // eslint-disable-line no-eval
   }
 
-  Array
-    .from(document.querySelectorAll("button[data-run]"))
-    .forEach(element => element.addEventListener("click", evalRunButton.bind(null, element)))
+  const buttons = Array.from(document.querySelectorAll("button[data-run]"))
+
+  buttons.forEach(element => element.addEventListener("click", evalRunButton.bind(null, element)))
+
+  evalRunButton(buttons[0])
+  const instanceElement = demoInstance.application.element
+
+  instanceElement.addEventListener("mouseover", stopDemoLoop)
+  instanceElement.addEventListener("click", stopDemoLoop)
 })
