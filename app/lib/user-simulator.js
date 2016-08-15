1f08c65a25d91c94c0fff3d6501c55d0f3db5e03
Teffen Ellis (Eric) <teffen@nirri.us>
2016-08-15 02:46:12 -0700

    Fix race conditions around simulator.

--- app/lib/user-simulator.js
+++ app/lib/user-simulator.js
@@ -1,34 +1,41 @@
 import keyMap from "lib/key-map"
 import smoothScroll from "smooth-scroll"
-const TARGET_ENTRIES = [
-  "WordPress",
-  "Drupal",
-  "Joomla",
-  "Any site"
-]
 
-let sequence = []
-let embedBox
+function createSequence(EmbedBox) {
+  const targetLabels = EmbedBox.fetchedTargets.map(Target => Target.label)
 
-const toKeyDown = () => ({entity: keyMap.down, eventType: "keydown"})
+  let sequence = []
 
-TARGET_ENTRIES.forEach(phrase => {
-  const subSet = [...phrase, keyMap.backspace]
+  const toKeyDown = () => ({entity: keyMap.down, eventType: "keydown"})
 
-  sequence = sequence.concat(subSet.map(entity => ({entity, eventType: "input"})))
-})
+  targetLabels.forEach(phrase => {
+    const subSet = [...phrase, keyMap.backspace]
 
-sequence = sequence.concat(
-  TARGET_ENTRIES.map(toKeyDown),
-  {entity: keyMap.enter, eventType: "keypress"}
-)
+    sequence = sequence.concat(subSet.map(entity => ({entity, eventType: "input"})))
+  })
 
-sequence.push({eventType: "scroll"})
+  sequence = sequence.concat(
+    targetLabels.map(toKeyDown),
+    {entity: keyMap.enter, eventType: "keypress"}
+  )
+
+  sequence.push({eventType: "scroll"})
+
+  return sequence
+}
+
+let instance
+let completionTimeout
+let stepTimeout
+let createInteractiveDemo
 
 export function runDemo(iframe, onComplete = () => {}) {
-  let completionTimeout
-  let stepTimeout
+  // Automated and user events may be subjected to race conditions between runs.
+  clearTimeout(stepTimeout)
+  clearTimeout(completionTimeout)
+
   const {EmbedBox} = iframe.contentWindow
+  const sequence = createSequence(EmbedBox)
   const DEFAULTS = {
     embedCode: "<script src='http://example.com/plugin.js'></script>",
     downloadURL: "about:blank",
@@ -38,14 +45,16 @@ export function runDemo(iframe, onComplete = () => {}) {
   const loadingDots = iframe.parentNode.querySelector(".loading-dots")
   let running = true
 
-  function createInteractiveDemo() {
+  if (createInteractiveDemo) barrier.removeEventListener("click", createInteractiveDemo)
+
+  createInteractiveDemo = () => {
     clearTimeout(stepTimeout)
     clearTimeout(completionTimeout)
     running = false
 
     barrier.style.display = "none"
 
-    if (embedBox) embedBox.destroy()
+    if (instance) instance.destroy()
 
     const config = {...DEFAULTS,
       events: {
@@ -63,22 +72,25 @@ export function runDemo(iframe, onComplete = () => {}) {
     }
 
     loadingDots.setAttribute("data-state", "loading")
-    requestAnimationFrame(() => embedBox = new EmbedBox(config))
+    requestAnimationFrame(() => instance = new EmbedBox(config))
   }
 
-  if (embedBox) embedBox.destroy()
-  embedBox = new EmbedBox(DEFAULTS)
+  if (instance) instance.destroy()
+  instance = new EmbedBox(DEFAULTS)
   loadingDots.setAttribute("data-state", "loaded")
 
   barrier.addEventListener("click", createInteractiveDemo)
 
-  const iframeDocument = embedBox.iframe.document
+  const iframeDocument = instance.iframe.document
   const searchComponent = iframeDocument.querySelector("[data-component='target-search']")
   const input = searchComponent.querySelector(".search")
 
+  // Prevent input events from scrolling Firefox.
+  input.readOnly = true
+
   function simulate(index = 0) {
     if (!running) return
-    if (["hiding", "hidden"].includes(embedBox.visibility)) return
+    if (["hiding", "hidden"].includes(instance.visibility)) return
 
     const {entity, eventType} = sequence[index]
     const meta = {}
