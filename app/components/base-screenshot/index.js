7908dca49dcaf826765f599f89260742fe4e544e
Teffen Ellis (Eric) <teffen@nirri.us>
2016-09-07 13:43:39 -0700

    Improve screenshot rendering in IE10. CC @adamschwartz
    - I suspect this isn't completely resolved but it's a step in the right direction.

--- app/components/base-screenshot/index.js
+++ app/components/base-screenshot/index.js
@@ -4,6 +4,8 @@ import iframeStylesheet from "./screenshot-iframe.styl"
 import BaseComponent from "components/base-component"
 import autobind from "autobind-decorator"
 
+const toPrecision = (number, precision = 5) => parseFloat(number.toPrecision(precision))
+
 export default class BaseScreenshot {
   static iframeTemplate = iframeTemplate;
   static iframeStylesheet = iframeStylesheet;
@@ -13,27 +15,29 @@ export default class BaseScreenshot {
   @autobind
   setScale() {
     const iframeDocument = this.iframe.contentDocument
-    const {width: widthStyle, height: heightStyle} = iframeDocument.defaultView.getComputedStyle(iframeDocument.body)
-    const width = parseInt(widthStyle)
-    const height = parseInt(heightStyle)
-    const intrinsicRatio = height / width
+    const {getComputedStyle} = iframeDocument.defaultView
+    const {width: widthStyle, height: heightStyle} = getComputedStyle(iframeDocument.body)
+    const width = parseInt(widthStyle, 10)
+    const height = parseInt(heightStyle, 10)
+    const intrinsicRatio = toPrecision(height / width)
     const paddingBottom = `${intrinsicRatio * 100}%`
-    const scale = this.element.clientWidth / width
 
     this.iframe.setAttribute("width", width)
     this.iframe.setAttribute("height", height)
     this.element.style.paddingBottom = paddingBottom
-    this.iframe.style.transform = `scale(${scale})`
 
-    this.element.setAttribute("data-render-state", "scaled")
+    requestAnimationFrame(() => {
+      const scale = toPrecision(this.element.clientWidth / width)
+
+      this.iframe.style.transform = `scale(${scale})`
+      this.element.setAttribute("data-render-state", "scaled")
+    })
   }
 
   render(target) {
     const {iframeTemplate, iframeStylesheet, stylesheet, template} = this.constructor
-
     const element = this.element = this.serialize(iframeTemplate)
     const iframe = this.iframe = element.querySelector("iframe")
-
 
     this.iframe.onload = () => {
       const iframeDocument = iframe.contentDocument
