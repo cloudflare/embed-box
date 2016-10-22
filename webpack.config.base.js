a028fca0c9178bacc7c1d3dcb30804c6a6ef1f11
Teffen Ellis (Eric) <teffen@nirri.us>
2016-10-22 01:39:14 -0700

    Remove redundant path attribute.

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -70,7 +70,7 @@ module.exports = function createWebpackConfig(overrides = {}) {
     loaders: loaders.concat([
       {test: /\.md$/, loader: "html!markdown", exclude},
       {test: /\.pug$/, loader: "pug", exclude},
-      {test: /\.png|jpe?g|gif$/i, loader: "file?name=assets/[path][name].[ext]", exclude: /site/},
+      {test: /\.png|jpe?g|gif$/i, loader: "file?name=[path][name].[ext]", exclude: /site/},
       {test: /\.js$/, loader: "babel", exclude},
       {test: /\.svg$/, loader: "svg-inline", exclude},
       {
