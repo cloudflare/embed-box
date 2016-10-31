67c6b13a17357d53c8fde65e0d32b194c49b7c1b
Teffen Ellis (Eric) <teffen@nirri.us>
2016-10-31 03:15:20 -0700

    Flesh out asset compression.

--- scripts/build-dist.js
+++ scripts/build-dist.js
@@ -2,12 +2,15 @@
 
 const baseConfig = require("../webpack.config.base")()
 const del = require("del")
-const {each} = require("async")
+const {each, eachOfSeries} = require("async")
 const webpack = require("webpack")
 const {resolve} = require("path")
 const {entries, IDs} = require("../modules")
 const validate = require("./validate")
+const {spawnSync} = require("child_process")
 const fs = require("fs")
+const {walkSync} = require("file")
+const zopflipng = require("zopflipng-bin")
 const zipFolder = require("zip-folder")
 
 const ASSET_ARCHIVE_NAME = "assets.zip"
@@ -67,11 +70,36 @@ function buildEntry(id, next) {
 
 console.log("Building bundles...")
 each(IDs, buildEntry, () => {
-  zipFolder(`${outputPath}/assets/`, `${outputPath}/${ASSET_ARCHIVE_NAME}`, error => {
-    if (error) {
-      console.error(error)
-      return
-    }
-    console.log(`+ ${ASSET_ARCHIVE_NAME}`)
+  const spawnOptions = {
+    cwd: resolve(".."),
+    encoding: "utf8",
+    env: process.env,
+    stdio: [0, 1, 2]
+  }
+
+  const assetPaths = []
+
+  walkSync(resolve(__dirname, "../dist/assets"), (path, _, filenames) => {
+    filenames.forEach(filename => assetPaths.push(`${path}/${filename}`))
+  })
+
+  console.log("Compressing assets... (this may take some time)")
+
+  function compressAsset(path, index, next) {
+    const {error} = spawnSync(zopflipng, ["-y", path, path], spawnOptions)
+
+    if (error) throw error
+    console.log(`+ [${index + 1}/${assetPaths.length}] ${path}`)
+    next()
+  }
+
+  eachOfSeries(assetPaths, compressAsset, () => {
+    zipFolder(`${outputPath}/assets/`, `${outputPath}/${ASSET_ARCHIVE_NAME}`, error => {
+      if (error) {
+        console.error(error)
+        return
+      }
+      console.log(`+ ${ASSET_ARCHIVE_NAME}`)
+    })
   })
 })
